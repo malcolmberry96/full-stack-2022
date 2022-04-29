efc1a34690a85b8b0b0f16ed701a606223948d85
Malcolm Berry <mlb22@rice.edu>
2022-04-29 14:19:39 -0500

    Finish 1.0 - 1.12 exercise

--- part1/src/App.js
+++ part1/src/App.js
@@ -35,7 +35,7 @@ return (
      <StatisticLine text={'Good '} value={props.good} />
      <StatisticLine text={'Neutral '} value={props.neutral} />
      <StatisticLine text={'Bad '} value={props.bad} />
-     <StatisticLine text={'All '} value={all} />
+     <StatisticLine text={'All '} value={props.all} />
      <StatisticLine text={'Average '} value={(good - bad) / all} />
      <StatisticLine text={'Percent Positive '} value={String(good/all * 100) + '%'} />
 
@@ -55,7 +55,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allReviews, setAll] = useState([])
 
+  const handleGoodReview = () => {
+    setAll(allReviews.concat('G'))
+    setGood(good + 1)
+  }
+
+  const handleNeutralReview = () => {
+    setAll(allReviews.concat('N'))
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadReview = () => {
+    setAll(allReviews.concat('B'))
+    setBad(bad + 1)
+  }
 
 
   return (
@@ -68,7 +83,7 @@ const App = () => {
     <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
     <br></br>
     <h2>Statistics</h2>
-    <Statistics name={'statistics'} good={good} neutral={neutral} bad={bad}/>
+    <Statistics name={'statistics'} good={good} neutral={neutral} bad={bad} allReviews={allReviews}/>
   </div>
 )
   }
